5afb4d826da7eabfdd2bbb92fae23a2b1a3028d6
Pavlyukovets <Ivan_Pavlyukovets@epam.com>
2016-05-20 20:30:30 +0300

    tests

--- dog-web/src/main/java/com/piv/config/AppConfig.java
+++ dog-web/src/main/java/com/piv/config/AppConfig.java
@@ -5,14 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 @ComponentScan("com.piv")
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Override
---
+++ dog-web/src/test/java/com/piv/endpoint/DogEndpointTest.java
@@ -0,0 +1,31 @@
+package com.piv.endpoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.piv.config.AppConfig;
+import com.piv.model.Dog;
+import com.piv.repositories.DogRepository;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.AssertJUnit.*;
+import static com.jayway.restassured.RestAssured.*;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = AppConfig.class)
+public class DogEndpointTest extends AbstractTestNGSpringContextTests {
+
+    @Test
+    public void mustGetValidCollection() throws IOException {
+        String dogBody = given().get("/dogs").andReturn().asString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Dog> dogs = objectMapper.readValue(dogBody, objectMapper.getTypeFactory().constructCollectionType(List.class, Dog.class));
+
+        assertEquals(DogRepository.DOGS, dogs);
+    }
+
+}
