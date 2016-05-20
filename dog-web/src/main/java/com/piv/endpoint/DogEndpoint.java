a6bec403da920a2d42e3de6aa9cc6eaea2fc682d
Pavlyukovets <Ivan_Pavlyukovets@epam.com>
2016-05-20 15:14:04 +0300

    start controller

--- dog-web/src/main/java/com/piv/endpoint/DogEndpoint.java
+++ dog-web/src/main/java/com/piv/endpoint/DogEndpoint.java
@@ -1,16 +1,21 @@
 package com.piv.endpoint;
 
+import com.piv.model.Dog;
 import com.piv.repositories.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
+import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class DogEndpoint {
     @Autowired
     private DogRepository dogRepository;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/dogs")
+    public List<Dog> getAllDogs() {
+        return dogRepository.getAllDogs();
+    }
 }
