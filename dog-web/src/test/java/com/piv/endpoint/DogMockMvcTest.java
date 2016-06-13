7e3155d89237ec499edbd0a4dea4920a67650884
Vanya Gonzalez <storekeeper@mail.ru>
2016-06-13 23:37:31 +0300

    test

--- dog-web/src/test/java/com/piv/endpoint/DogMockMvcTest.java
+++ dog-web/src/test/java/com/piv/endpoint/DogMockMvcTest.java
@@ -5,6 +5,6 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 @Test
-@ContextConfiguration(locations = { "classpath:/mvc-dispatcher-servlet.xml" })
+@ContextConfiguration(locations = { "classpath:mvc-dispatcher-servlet.xml" })
 public class DogMockMvcTest extends AbstractTestNGSpringContextTests {
 }
